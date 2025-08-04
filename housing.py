1798e669dfb1d4c3dda646f55c7ec96ab2277b4c
mysterI0s <jaafer.mahfoud97@gmail.com>
2025-08-04 13:28:47 +0300

    a few edits

--- housing.py
+++ housing.py
@@ -3,6 +3,9 @@ import tarfile
 import urllib.request
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -24,8 +27,34 @@ def load_housing_data(housing_path=HOUSING_PATH):
     return pd.read_csv(csv_path)
 
 
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
 if __name__ == "__main__":
     housing = load_housing_data()
+    housing["income_cat"] = pd.cut(
+        housing["median_income"],
+        bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+        labels=[1, 2, 3, 4, 5],
+    )
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+    print(len(strat_train_set), "train +", len(strat_test_set), "test")
+    print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
     print(housing.head())
     print(housing.info())
+    print(housing["ocean_proximity"].value_counts())
+    print(housing.describe())
+    housing.hist(bins=50, figsize=(20, 15))
+    plt.show()
+    housing["income_cat"].hist()
     plt.show()
